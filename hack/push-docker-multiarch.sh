399c10035793f86ff6ffb5c058d36b0a47825bc0
Kyle King <kyle.king@shaw.ca>
2024-01-14 05:23:15 -0800

    Add image tag args to docker build call
    This creates a bash array of tag arguments (`--tag TAG`) for use in the docker build call.
    
    This allows for combining the build and push command (required for pushing multi-arch images with `docker buildx build`)

--- hack/push-docker-multiarch.sh
+++ hack/push-docker-multiarch.sh
@@ -3,18 +3,16 @@
 set -euxo pipefail
 
 IMAGE_NAME="irvinlim/apple-health-ingester"
-
-docker build -t "${IMAGE_NAME}:latest" .
+IMAGE_TAG_ARGS=(--tag "${IMAGE_NAME}:latest")
 
 # Use git commit hash for untagged git builds
 COMMIT_TAG=$(git rev-parse HEAD | cut -c1-12)
-docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${COMMIT_TAG}"
-docker push "${IMAGE_NAME}:latest"
-docker push "${IMAGE_NAME}:${COMMIT_TAG}"
+IMAGE_TAG_ARGS+=(--tag "${IMAGE_NAME}:${COMMIT_TAG}")
 
-# Also push with additional tag if git tag is set
+# Include additional tag if git tag is set
 GIT_TAG=$(git tag --points-at HEAD)
 if [[ -n "${GIT_TAG}" ]]; then
-  docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${GIT_TAG}"
-  docker push "${IMAGE_NAME}:${GIT_TAG}"
+  IMAGE_TAG_ARGS+=(--tag "${IMAGE_NAME}:${GIT_TAG}")
 fi
+
+docker build "${IMAGE_TAG_ARGS[@]}" --push .
